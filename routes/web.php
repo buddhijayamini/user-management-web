57bb70dda9afaaf9bfae60a51b610c19547ac992
buddhijayamini <buddhigeethen@gmail.com>
2022-08-05 10:03:02 +0530

    nortification added...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\StatusLiked;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('test', function () {
+	event(new StatusLiked('Admin'));
+	return "Event has been sent!";
 });
